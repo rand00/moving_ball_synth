7fe41a0e5003e6ba851dbf2c56a2000c516c8df3
rand00 <oth.rand@gmail.com>
2015-04-01 00:22:40 +0200

    Updated init with server-init-check

--- src/main.ml
+++ src/main.ml
@@ -3,10 +3,12 @@ open Batteries
 open Ball
 
 let init () = 
-  let _ = SC.Server.run_script () in 
-  let c = SC.Client.make () in
-  let _ = at_exit (fun () -> SC.Client.quit_all c)
-  in c
+  match SC.Server.run_script () with
+  | false -> failwith "Ball: SuperCollider server (scsynth) failed to start."
+  | true -> 
+    let c = SC.Client.make () in
+    let _ = at_exit (fun () -> SC.Client.quit_all c)
+    in c 
 
 let test_01 () =
   let freq_base = 
@@ -15,8 +17,8 @@ let test_01 () =
     else 300 
   in
   let c = init () in
-  let s = s_sinew c in
-
+  let s = s_sinew c 
+  in
   while true do 
     match_xyz (read_line ()) 
     >>| apply_x (bounce_minval freq_base)
