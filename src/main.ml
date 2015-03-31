48d7d103e6ecf211a6a58f3555d7e3f9d251b119
rand00 <oth.rand@gmail.com>
2015-03-31 19:09:34 +0200

    Factored out common client/server initialization and first test-usage of IMU-output

--- src/main.ml
+++ src/main.ml
@@ -2,17 +2,20 @@ open Batteries
 
 open Ball
 
-let _ =
+let init () = 
+  let _ = SC.Server.run_script () in 
+  let c = SC.Client.make () in
+  let _ = at_exit (fun () -> SC.Client.quit_all c)
+  in c
 
+let test_01 () =
   let freq_base = 
     if Array.length Sys.argv > 1 then 
       Int.of_string(Sys.argv.(1)) 
     else 300 
   in
-  let _ = SC.Server.run_script () in 
-  let c = SC.Client.make () in
+  let c = init () in
   let s = s_sinew c in
-  at_exit (fun () -> SC.Client.quit_all c);
 
   while true do 
     match_xyz (read_line ()) 
@@ -20,5 +23,9 @@ let _ =
     >>| Int.mul 2
     >>| set_freq s
     |> ignore
-  done;
+  done
+
+
+let () = test_01 ()
+
 
