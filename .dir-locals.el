58656f2df6d633fe311ee8183f230bc8218ca79e
rand00 <oth.rand@gmail.com>
2015-03-31 12:24:43 +0200

    Added local configs for loading code live in REPL (need to move camlp4-using funcs out into compiled module)

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((tuareg-mode .
+              ((utop-command . "utop -init /home/rand/code/installations/moving_ball_synth/.ocamlinit")
+               (compile-command . "make -C /home/rand/code/installations/moving_ball_synth"))))
