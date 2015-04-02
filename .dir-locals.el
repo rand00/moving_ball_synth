d0b0ff05d96a1dd2e4edb9aeb3441eb03b660925
rand00 <oth.rand@gmail.com>
2015-04-03 00:40:28 +0200

    Not going to use .dir-locals.el now

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((tuareg-mode .
-              ((utop-command . "utop -init /home/rand/code/installations/moving_ball_synth/.ocamlinit")
-               (compile-command . "make -C /home/rand/code/installations/moving_ball_synth"))))
---
+++ .dir-locals_orig.el
@@ -0,0 +1,2 @@
+((tuareg-mode . ((utop-command . "utop -init /home/rand/code/installations/moving_ball_synth/.ocamlinit")
+                 (compile-command . "make -C /home/rand/code/installations/moving_ball_synth"))))
