d4801f89771026c15e8d4db65239ce359a5e542d
rand00 <oth.rand@gmail.com>
2015-04-03 01:38:18 +0200

    a fake IMU test script

---
+++ fake_imu.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+while true
+do 
+    echo "X: 0 Y: 0 Z: 0"
+    sleep 1
+    echo "X: 100 Y: 100 Z: 100"
+    sleep 1
+    echo "X: 20 Y: 40 Z: 40"
+    sleep 1
+    echo "X: 23 Y: 20 Z: 20"
+    sleep 1
+    echo "X: 15 Y: 10 Z: 80"
+    sleep 1
+done
