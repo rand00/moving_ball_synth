53017674406cf5e27661b5595c9062e575e26423
rand00 <oth.rand@gmail.com>
2015-04-03 00:39:59 +0200

    Added new loop, datatypes and range mapping of values

--- src/main.ml
+++ src/main.ml
@@ -21,13 +21,91 @@ let test_01 () =
   in
   while true do 
     match_xyz (read_line ()) 
-    >>| apply_x (bounce_minval freq_base)
-    >>| Int.mul 2
-    >>| set_freq s
-    |> ignore
+    >>| (fun (x, y, z) -> 
+        let x' = bounce_minval freq_base x 
+        in S.set s [( "freq", `I x')]
+      ) |> ignore
   done
 
+type value = {
+  min : float;
+  max : float;
+  out_min : float;
+  out_max : float;
+  prev : float;
+}
 
-let () = test_01 ()
+type input_state = {
+  x : value;
+  y : value;
+  z : value;
+}
+
+let set_min_max v_wrap v = {
+  v_wrap with
+  min = if v < v_wrap.min then v else v_wrap.min;
+  max = if v > v_wrap.max then v else v_wrap.max
+}
+
+let map_range_3 st x y z = 
+  let aux v_wrap v = 
+    map_range 
+      (v_wrap.min, v_wrap.max)
+      (v_wrap.out_min, v_wrap.out_max)
+      (x) 
+  in 
+  let x' = aux st.x x
+  and y' = aux st.y y
+  and z' = aux st.z z
+  in (x', y', z')
+  
+
+let rec loop st s =
+  read_line () |> match_xyz
+  |> function 
+  | None -> loop st s
+  | Some (x, y, z) -> 
+    let x, y, z = (float x), (float y), (float z) in
+    let st = { 
+      x = set_min_max st.x x;
+      y = set_min_max st.y y;
+      z = set_min_max st.z z;
+    } in
+    let x', y', z' = map_range_3 st x y z 
+    in
+    let _ = S.set s [
+        ("lfo_freq", `F x'); 
+        ("freq", `F y'); 
+        ("imp_freq", `F z')
+      ] 
+    in
+    let st = { 
+      x = { st.x with prev = x' };
+      y = { st.y with prev = y' };
+      z = { st.z with prev = z' };
+    } 
+    in loop st s
+
+let std_values = {
+  min = 0.;
+  max = 1.;
+  out_min = 0.;
+  out_max = 100.;
+  prev = 0.;
+}
+
+let run_3_mapped_ranges () =
+  let c = init () in
+  let s = s_sinew c in
+  let st = {
+    x = { std_values with out_min = 0.01; out_max = 300. };
+    y = { std_values with out_min = 30.; out_max = 1100. };
+    z = { std_values with out_min = 10.; out_max = 80. };
+  } 
+  in loop st s
+
+
+
+let () = run_3_mapped_ranges ()
 
 
